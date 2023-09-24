98203469f8948ce8a9dc3e9d0cdbe01017add59e
Ludivine <mrt.ludivine@gmail.com>
2023-09-24 15:53:17 +0200

    creation d'un composant dynamique

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PokemonCard from "./components/PokemonCard"
+import "./App.css";
 
 function App() {
   return (
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,42 @@
 function PokemonCard() {
+  const pokemon = "mew";
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const PokemonCard = (name) => {
+    const Pokemondata = pokemonList.find(pokemon => pokemon.name === name);
+    
     return (
       <div>
-        <figure>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"></img>
-        </figure>
-        <figcaption>
-          <h1>Bulbasaur</h1>
-        </figcaption>
+        {Pokemondata && (
+          <>
+            <img src={Pokemondata.imgSrc} alt={Pokemondata.name}/>
+            <figcaption>{Pokemondata.name}</figcaption>
+          </>
+        )}
+        {!Pokemondata && (
+          <div>
+            <figcaption>???</figcaption>
+          </div>
+        )}
       </div>
     );
-  }
-  
-  export default PokemonCard;
-  
+  };
+
+  return (
+    <figure className="card">
+      {PokemonCard(pokemon)}
+    </figure>
+  );
+}
+
+export default PokemonCard;
